297cbf59b4cb4d77106afd7acccac03f5e46921e
Igor Lyubimov <HarryLyu@users.noreply.github.com>
2020-07-15 16:34:51 -0400

    init

---
+++ test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Честно говоря, непонятна формулировка "Выложите куда-нибудь код “Markeaze for me!” на Python"
+# Что-то типа Hello world?
+companyName = 'Markeaze'
+
+print('{0} for me!'.format(companyName))
